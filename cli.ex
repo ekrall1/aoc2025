0ed56d7a31e180ad17ecccf8285763c6fd18cf11
Eric Krall <krall.eric@gmail.com>
2025-09-14 11:35:28 -0400

    setting up for elixir

---
+++ cli.ex
@@ -0,0 +1,5 @@
+defmodule Aoc2025.CLI do
+  def main(args) do
+    IO.puts("Advent of Code 2025 starting… args: #{inspect(args)}")
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule Aoc2025.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aoc2025,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      escript: [main_module: Aoc2025.CLI],
+      deps: []
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+end
