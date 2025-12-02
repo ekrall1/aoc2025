da8481c90e3778d37e0b34de843e3a3da25daca7
Eric Krall <krall.eric@gmail.com>
2025-12-02 02:46:25 -0500

    day 1 part 2

--- lib/aoc2025/days/day01.ex
+++ lib/aoc2025/days/day01.ex
@@ -24,15 +24,17 @@ defmodule Aoc2025.Days.Day01 do
 
   @spec part2(any()) :: <<_::256>>
   @doc """
-  Solves part 2 of day 1.
+     iex> input = File.read!("tests/test_input/day01.txt")
+     iex> Aoc2025.Days.Day01.part2(input)
+     "6"
   """
   @impl Aoc2025.Day
   def part2(input) do
-    # TODO: Implement Day 1 Part 2
-    # input is the raw file content as a string
-    "Day 1 Part 2 not implemented yet"
-  end
+    lst = parse_input(input)
+    {_, zeros} = p2_reduce(lst, 50, 100)
 
+    Integer.to_string(zeros)
+  end
 
   defp parse_input(input) do
     input
@@ -48,8 +50,39 @@ defmodule Aoc2025.Days.Day01 do
         if dir == "R",
           do: Integer.mod(position + spaces, nums),
           else: Integer.mod(position - spaces, nums)
+
       zeros = if position == 0, do: zeros + 1, else: zeros
       {position, zeros}
     end)
+  end
+
+  defp p2_reduce(lst, start, nums) do
+    Enum.reduce(lst, {start, 0}, fn {dir, spaces}, {position, zeros} ->
+      move = if dir == "R", do: spaces, else: -spaces
+
+      crossings = get_p2_crossings(position, move, nums)
+
+      position = Integer.mod(position + move, nums)
+      {position, zeros + crossings}
+    end)
+  end
+
+  defp get_p2_crossings(position, move, nums) do
+    cond do
+      move > 0 ->
+        div(position + move, nums)
+
+      move < 0 ->
+        d = -move
+
+        cond do
+          d < position -> 0
+          position == 0 -> div(d, nums)
+          true -> div(d + nums - position, nums)
+        end
+
+      true ->
+        0
+    end
   end
 end
