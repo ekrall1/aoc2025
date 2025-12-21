659fd7336fc5c2c75da07d2d99df53c46759d3cc
Eric Krall <krall.eric@gmail.com>
2025-12-21 09:04:29 -0500

    day 10 part 2

--- lib/aoc2025/days/day10.ex
+++ lib/aoc2025/days/day10.ex
@@ -55,7 +55,7 @@ defmodule Aoc2025.Days.Day10 do
   ## Examples
 
       iex> test_input = File.read!("tests/test_input/day10.txt")
-      iex> Aoc2025.Days.Day10.part1(test_input)
+      iex> Aoc2025.Days.Day10.part2(test_input)
       "33"
 
   """
